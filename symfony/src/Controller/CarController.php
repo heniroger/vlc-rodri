76c6f5096b4c0a85b981b86ca30710d26deba2c2
heniroger <heniroger@yahoo.fr>
2022-02-24 22:10:04 +0300

    Crud Car comment user

---
+++ symfony/src/Controller/CarController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Car;
+use App\Entity\Comment;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CarController extends AbstractController{
+
+    private $managerRegistry;
+    private $em;
+
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        EntityManagerInterface $em
+    ){
+        $this->managerRegistry = $managerRegistry;
+        $this->em = $em;
+    }
+
+    public function add(Request $request){
+
+        $data = json_decode($request->getContent(),true);
+
+        if(
+            !isset($data['photo'])
+            && !isset($data['mark'])
+            && !isset($data['description'])
+        ){
+
+            return new JsonResponse([
+                "status" => Response::HTTP_FORBIDDEN,
+                "message" => "Some data is missing"
+            ], Response::HTTP_FORBIDDEN);
+        }
+        $photo = $data['photo'];
+        $mark = $data['mark'];
+        $description = $data['description'];
+
+        $manager = $this->managerRegistry->getManager();
+
+        /**
+        * @var  User
+        */
+        $user = $this->getUser();
+
+        $car = new Car();
+        $car->setPhoto($photo);
+        $car->setMark($mark);
+        $car->setDescription($description);
+        $car->setCreator($user);
+
+        $manager->persist($car);
+        $manager->flush();
+
+         return new JsonResponse([
+                "status" => Response::HTTP_OK,
+                "message" => "Success",
+                "data" => [
+                    'photo' => $photo,
+                    'mark' => $mark,
+                    'description' => $description,
+                ]
+        ], Response::HTTP_OK);
+    }
+
+    public function list(){
+
+    
+        $cars = $this->em->getRepository(Car::class)->findAll();
+
+        $data = [];
+
+        foreach($cars as $car){
+            if($car instanceof Car){
+
+                $aCar = [
+                    'photo' => $car->getPhoto(),
+                    'mark' => $car->getMark(),
+                    'description' => $car->getDescription(),
+                    'createdAt' => $car->getCreatedAt()->format('Y-m-d H:i:s'),
+                    'owner' => [
+                        'firstName' => $car->getCreator()->getFirstName(),
+                        'lastName' => $car->getCreator()->getLastName(),
+                    ],
+                ];
+                $aCar['comments'] = []; 
+                foreach($car->getComments() as $comment){
+                    if($comment instanceof Comment){
+                        $aCar['comments'][] = [
+                            'content' => $comment->getContent(),
+                            'createdAt' => $comment->getCreatedAt()->format('Y-m-d H:i:s'),
+                        ];
+                    }
+                }
+
+                $data[] = $aCar;
+            }
+        }
+        return new JsonResponse([
+            "status" => Response::HTTP_OK,
+            "message" => "Success",
+            "data" => $data
+        ], Response::HTTP_OK);
+    }
+
+
+    public function addComment(Request $request, int $id){
+        $car = $this->em->getRepository(Car::class)->find($id);
+
+        if(!$car){
+             return new JsonResponse([
+                "status" => Response::HTTP_NOT_FOUND,
+                "message" => "Car not found!",
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $comment= new Comment();
+        $comment->setContent($data['content']);
+        $comment->setCar($car);
+        $car->getComments()->add($comment);
+        $comment->setUser($this->em->getRepository(User::class)->find($data['userId']));
+
+        return new JsonResponse([
+            "status" => Response::HTTP_OK,
+            "message" => "Success",
+        ], Response::HTTP_OK);
+
+    }
+}
